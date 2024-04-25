28870b3b888d2fc815c1865330177a2bc91c31a9
nuca19 <nunocardosopinho@gmail.com>
2024-04-25 16:46:00 +0100

    search bar funcional

--- couchcritics/app/components/Navbar.tsx
+++ couchcritics/app/components/Navbar.tsx
@@ -1,21 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass, faUser } from '@fortawesome/free-solid-svg-icons';
 import Link from 'next/link';
+import SearchBar from './SearchBar.client';
 
 const Navbar = () => {
   return (
     <nav className="flex justify-between w-full p-20 px-60">
       <Link href="/"><h1 className="text-2xl font-bold">Couch Critics</h1></Link>
       <div className="flex space-x-20 items-baseline" >
-      <Link href="/movies">Movies</Link>
-      <Link href="/tvshows">TV Shows</Link>
-        <input type="checkbox" id="searchToggle" className="hidden" />
-        <label htmlFor="searchToggle">
-          <FontAwesomeIcon icon={faMagnifyingGlass} style={{ width: '1em', height: '1em' }} />
-        </label>
-        <input type="text" id="searchBar" className="hidden" placeholder="Search..." />
-        <a href="/Account">
+        <Link href="/movies">Movies</Link>
+        <Link href="/tvshows">TV Shows</Link>
+        <SearchBar />
+        <a href="/account">
           <FontAwesomeIcon icon={faUser} style={{ width: '1em', height: '1em'}} />
         </a>
       </div>
---
+++ couchcritics/app/components/SearchBar.client.tsx
@@ -0,0 +1,61 @@
+'use client'
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import PocketBase from 'pocketbase';
+
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isSearchVisible, setSearchVisible] = useState(false);
+
+  async function getSearch(searchTerm: string){
+    try {
+      const pb = new PocketBase('http://127.0.0.1:8090')
+      const resultList = await pb.collection('movies').getList(1, 1, { filter: pb.filter("title ~ {:title} ", { title: searchTerm}) });
+      return resultList?.items as any[];
+    } catch (error) {
+      console.error('An error occurred while fetching', error);
+      return [];
+    }
+  }
+  
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    console.log(`Searching for ${searchTerm}`);
+    getSearch(searchTerm).then((result) => {
+      console.log(result[0].title);
+    });
+
+  };
+
+  return (
+<div style={{ display: 'flex', alignItems: 'center' }}>
+  <input 
+    type="checkbox" 
+    id="searchToggle" 
+    className="hidden" 
+    checked={isSearchVisible}
+    onChange={() => setSearchVisible(!isSearchVisible)}
+  />
+  <label htmlFor="searchToggle">
+    <FontAwesomeIcon icon={faMagnifyingGlass} style={{ width: '1em', height: '1em', marginRight: '23px' }} />
+  </label>
+  {isSearchVisible && (
+    <form onSubmit={handleSearch} style={{ display: 'flex', alignItems: 'center' }}>
+      <input 
+        type="text" 
+        id="searchBar" 
+        placeholder="Search..." 
+        value={searchTerm} 
+        onChange={e => setSearchTerm(e.target.value)} 
+        style={{ width: '200px', height: '30px', border: '1px solid black', marginLeft: '10px' }}
+      />
+      <button type="submit"></button>
+    </form>
+  )}
+</div>
+  );
+};
+
+export default SearchBar;
