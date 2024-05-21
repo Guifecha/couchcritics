5f9b8b53c2e127c0acd37f13c27425f8dc944287
nuca19 <nunocardosopinho@gmail.com>
2024-05-21 16:29:46 +0100

    ups

--- couchcritics/app/tvshows/page.tsx
+++ couchcritics/app/tvshows/page.tsx
@@ -4,31 +4,8 @@ import PocketBase from 'pocketbase';
 import Link from 'next/link';
 import FilterAndOrderTvshows from '../components/FilterAndOrderTvshows';
 
-export const dynamic = 'auto',
-  dynamicParams = true,
-  revalidate = 0,
-  fetchCache = 'auto',
-  runtime = 'nodejs',
-  preferredRegion = 'auto'
-
-
-async function getTvShows(){
-  try{
-    const pb = new PocketBase('http://127.0.0.1:8090')
-    const resultList = await pb.collection('tvshows').getList(1, 50, {
-    });
-    return resultList?.items as any[];
-  } catch (error) {
-    console.error('An error occurred while fetching', error);
-    return [];
-  }
-}
-
-// fetch a paginated records list
-
 
 export default async function tvshows(){
-  const tvshows = await getTvShows();
 
   return (
     <main className="flex min-h-screen flex-col items-center">
